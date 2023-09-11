4ca5c083408918af63ad7688510d78f4cb6e468b
Gaspare <Gas9220@users.noreply.github.com>
2023-09-11 14:30:00 +0200

    Book class definition

---
+++ csharp-biblioteca/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class Book : Document
+    {
+        public int NumberOfPages { get; private set; }
+        public Book(int iD, string title, string year, Subject subject, string shelf, Author author, int numberOfPages) : base(iD, title, year, subject, shelf, author)
+        {
+            this.NumberOfPages = numberOfPages;
+        }
+    }
+}
