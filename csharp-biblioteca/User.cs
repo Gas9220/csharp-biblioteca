749738860b65ca7054146f080d83ba6aaefcb634
Gaspare <Gas9220@users.noreply.github.com>
2023-09-11 14:11:14 +0200

    Aggiungere i file di progetto.

---
+++ csharp-biblioteca/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class User
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        public string PhoneNumber { get; private set; }
+
+        public User(string firstName, string lastName, string email, string password, string phoneNumber)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Email = email;
+            this.Password = password;
+            this.PhoneNumber = phoneNumber;
+        }
+    }
+}
