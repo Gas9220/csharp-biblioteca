9d7ba053dec870d28089c7806dcc72219e196588
Gaspare <Gas9220@users.noreply.github.com>
2023-09-11 16:05:28 +0200

    Rent class created

---
+++ csharp-biblioteca/Rent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class Rent
+    {
+        public User User { get; private set; }
+        public DateTime StartDate {  get; private set; }
+        public DateTime EndDate { get; private set; }
+
+        public Rent(User user, DateTime startDate, DateTime endDate) {
+            this.User = user;
+            this.StartDate = startDate;
+            this.EndDate = endDate;
+        }
+    }
+}
