0863c5a6d9f9569902b2d235a9f8d07086f215c9
Gaspare <Gas9220@users.noreply.github.com>
2023-09-11 14:32:29 +0200

    Dvd class definition

---
+++ csharp-biblioteca/Dvd.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class Dvd: Document
+    {
+        public int Duration {  get; private set; }
+        public Dvd(int iD, string title, string year, Subject subject, string shelf, Author author, int duration) : base(iD, title, year, subject, shelf, author)
+        {
+            this.Duration = duration;
+        }
+
+    } 
+}
